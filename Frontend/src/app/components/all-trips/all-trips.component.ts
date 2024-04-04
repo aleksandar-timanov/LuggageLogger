2bcd16dc1f0a7ffb92dde9610a66de0067d6e63f
Aleksandar Timanov <aleksandar.timanov@rwth-aachen.de>
2024-04-04 21:34:56 +0200

    [all-trips](WIP): styling of cards grid

--- Frontend/src/app/components/all-trips/all-trips.component.ts
+++ Frontend/src/app/components/all-trips/all-trips.component.ts
@@ -1,45 +1,44 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'
+import { Component } from '@angular/core'
 
 @Component({
-  selector: 'app-all-trips',
-  standalone: true,
-  imports: [],
-  templateUrl: './all-trips.component.html',
-  styleUrl: './all-trips.component.scss'
+    selector: 'app-all-trips',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './all-trips.component.html',
+    styleUrl: './all-trips.component.scss',
 })
 export class AllTripsComponent {
-
-  temporaryData: any = [
-    {
-      "destination": "Sofia",
-      "departureDate": new Date(2024, 7, 15),
-      "returnDate ": new Date(2024, 7, 15)
-    },
-    {
-      "destination": "Sofia",
-      "departureDate": new Date(2024, 7, 15),
-      "returnDate ": new Date(2024, 7, 15)
-    },
-    {
-      "destination": "Sofia",
-      "departureDate": new Date(2024, 7, 15),
-      "returnDate ": new Date(2024, 7, 15)
-    },
-    {
-      "destination": "Sofia",
-      "departureDate": new Date(2024, 7, 15),
-      "returnDate ": new Date(2024, 7, 15)
-    },
-    {
-      "destination": "Sofia",
-      "departureDate": new Date(2024, 7, 15),
-      "returnDate ": new Date(2024, 7, 15)
-    },
-    {
-      "destination": "Sofia",
-      "departureDate": new Date(2024, 7, 15),
-      "returnDate ": new Date(2024, 7, 15)
-    }
-  ]
-
+    temporaryData: any = [
+        {
+            destination: 'Sofia',
+            departureDate: new Date(2024, 7, 15),
+            returnDate: new Date(2024, 7, 15),
+        },
+        {
+            destination: 'Sofia',
+            departureDate: new Date(2024, 7, 15),
+            returnDate: new Date(2024, 7, 15),
+        },
+        {
+            destination: 'Sofia',
+            departureDate: new Date(2024, 7, 15),
+            returnDate: new Date(2024, 7, 15),
+        },
+        {
+            destination: 'Sofia',
+            departureDate: new Date(2024, 7, 15),
+            returnDate: new Date(2024, 7, 15),
+        },
+        {
+            destination: 'Sofia',
+            departureDate: new Date(2024, 7, 15),
+            returnDate: new Date(2024, 7, 15),
+        },
+        {
+            destination: 'Sofia',
+            departureDate: new Date(2024, 7, 15),
+            returnDate: new Date(2024, 7, 15),
+        },
+    ]
 }
